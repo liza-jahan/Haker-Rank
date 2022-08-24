68c57994ee0883e41cbbe4ae56ef907adecc6be5
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2022-08-25 01:48:14 +0600

    Add files via upload

---
+++ Handshake.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int t,a,n,sum;
+   cin>>t;
+   while(t--)
+    {
+      cin>>n;
+
+      a=(n*(n-1))/2;
+
+        cout<<a<<endl;
+    }
+
+
+
+  return 0;
+}
